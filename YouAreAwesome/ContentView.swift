4b286eae707b6fba222abbcc13f90a60f41b8634
AquaticTurtle <goatzilla2021@gmail.com>
2025-02-10 14:18:30 -0800

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Student2 on 1/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var message = "I am a programmer!"
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+                .foregroundStyle(.black)
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome!") {
+                    message = "Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.orange)
+                .foregroundStyle(.white)
+                
+                
+                Button("Great!") {
+                    message = "Great!"
+                }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.orange)
+                .foregroundStyle(.white)
+                
+            }
+            
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
